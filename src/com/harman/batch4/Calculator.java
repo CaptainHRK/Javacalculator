e9e823cd301e6a2c5181beac6a9da39d1f821821
CaptainHRK <hrishikeshkhadkkkar3700@gmai.com>
2022-03-15 16:51:09 +0530

    addition and subtraction

--- src/com/harman/batch4/Calculator.java
+++ src/com/harman/batch4/Calculator.java
@@ -4,21 +4,23 @@ import java.util.Scanner;
 
 public class Calculator {
 
-    public int AdditionOperation(int x, int y)
-    {
-        int z=x+y;
-                return z;
-    }
+
+
     public static void main(String[] args) {
 
         Calculator calc=new Calculator();//object creation
+        Addition adCalc=new Addition();
+        Subtraction subCalc=new Subtraction();
+
         Scanner input = new Scanner(System.in);
-        int n1, n2, result;
+        int n1, n2, result1, result2;
         System.out.println("Enter n1= ");
         n1 = input.nextInt();
         System.out.println("Enter n2= ");
         n2 = input.nextInt();
-        result = n1 + n2;
-        System.out.println(result);
-    }
+        result1 = adCalc.AdditionOperation(n1,n2);
+        result2 = subCalc.SubOperation(n1,n2);
+        System.out.println(result1);
+        System.out.println(result2);
+
 }
---
+++ src/com/harman/batch4/Subtraction.java
@@ -0,0 +1,8 @@
+package com.harman.batch4;
+
+public class Subtraction {
+
+    public int SubOperation(int x,int y){
+        return x-y
+    }
+}
