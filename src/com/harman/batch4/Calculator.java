88fd9ecfedd1fd21d0e67f1711be48fc66eb9329
CaptainHRK <hrishikeshkhadkkkar3700@gmai.com>
2022-03-15 15:54:42 +0530

    Addition complete

--- src/com/harman/batch4/Calculator.java
+++ src/com/harman/batch4/Calculator.java
@@ -3,8 +3,15 @@ package com.harman.batch4;
 import java.util.Scanner;
 
 public class Calculator {
+
+    public int AdditionOperation(int x, int y)
+    {
+        int z=x+y;
+                return z;
+    }
     public static void main(String[] args) {
 
+        Calculator calc=new Calculator();//object creation
         Scanner input = new Scanner(System.in);
         int n1, n2, result;
         System.out.println("Enter n1= ");
